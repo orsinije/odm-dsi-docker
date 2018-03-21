8271f077e96e43efb8c8eb2620265ad612c45c48
Jean-Philippe Orsini <orsinije@fr.ibm.com>
2018-03-21 14:03:16 +0100

    wait until DSI is online instead of relying on a sleep

--- dsi-runtime/samples/simple/run.sh
+++ dsi-runtime/samples/simple/run.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
+function isonline() {
+        SOL_MANAGER_OPTS="--host=$1 --sslProtocol=TLSv1.2 --disableServerCertificateVerification=true --disableSSLHostnameVerification=true --username=tester --password=tester"
+        docker-compose run dsi-runtime /dsi-cmd serverManager isonline $SOL_MANAGER_OPTS
+}
+
 SRC_DIR=`dirname $0`
 SAMPLES_DIR=$SRC_DIR/..
 
 echo "Starts single DSI Runtime"
 pushd $SAMPLES_DIR
 ./dsi-single-run.sh
+CONTAINER_ID=`docker-compose ps -q`
+echo "DSI container: $CONTAINER_ID"
 popd
 
-echo "Waiting 60s to ensure that DSI runtime is ready"
-sleep 60
+DSI_IP=`docker exec $CONTAINER_ID hostname -I`
+echo "DSI IP: $DSI_IP"
 
-DSI_IP=`docker exec samples_dsi-runtime_1 hostname -I`
-echo "IP of the DSI server: $DSI_IP"
+# wait until DSI is ready
+until [ "$ISONLINE" == "1" ]
+do
+        echo "Waiting 5s before checking that DSI runtime is online"
+        sleep 5
+        ISONLINE=`isonline $DSI_IP >/dev/null && echo 1 || echo 0`
+        echo "Is online result: $ISONLINE"
+done
 
 echo "Deploys solution"
 $SRC_DIR/solution_deploy.sh $DSI_IP 9443
 
-echo "Waiting 30s to ensure that the solution and the connectivity configuration is deployed"
+echo "Waiting 30s to ensure that the solution and the connectivity configuration are deployed"
 sleep 30
 
 echo "Create a person"
