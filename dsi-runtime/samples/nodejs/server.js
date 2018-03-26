2d33fecdb0aa549f4e3d121d9ad1b176ee26f609
Jean-Philippe Orsini <orsinije@fr.ibm.com>
2018-03-26 10:27:18 +0200

    cleanup + run.sh deploy the solution when DSI is ready

--- dsi-runtime/samples/nodejs/server.js
+++ dsi-runtime/samples/nodejs/server.js
@@ -1,15 +1,48 @@
 'use strict';
 
 const express = require('express');
+const request = require('request');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
+const DSI_HOST = "dsi-runtime";
+
+function createEvent(name) {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+               + "<m:CreatePerson xmlns:m=\"http://www.ibm.com/ia/xmlns/default/SimpleSolution/model\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.ibm.com/ia/xmlns/default/SimpleSolution/model ../xsd/namespace1/model.xsd \">"
+               + "<m:name>" + name + "</m:name>"
+               + "<m:description>"
+               + "</m:description>"
+               + "</m:CreatePerson>";
+}
+
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+
+app.get('/in/*', (req, res) => {
+        var p = req.path.substring("/in/".length);
+        res.send('Name=' + p);
+
+        var url = "https://" + DSI_HOST + ":9443/in/simple";
+        console.log("POST to " + url);
+
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+        request.post({
+                        url: url,
+                        method: 'POST',
+                        headers: {
+                                'Content-Type' : 'application/xml'
+                        },
+                        body: createEvent(p)
+                },
+                function (err, response, body) {
+                        if (err) { console.log(err); }
+                        console.log(response);
+                        console.log(body.url);
+                        console.log(body.explanation);
+                });
 });
 
 app.listen(PORT, HOST);
--- dsi-runtime/samples/nodejs/solution_deploy.sh
+++ dsi-runtime/samples/nodejs/solution_deploy.sh
@@ -24,6 +24,8 @@ echo "Directory containing the solution: $SOL_DIR"
 
 cd $SRC_DIR
 
+echo "Deploy to DSI: $DSI_IP:$DSI_PORT"
+
 docker-compose run -v $SOL_DIR:/dropins dsi-runtime /dsi-cmd solutionManager deploy remote /dropins/simple_solution-0.0.esa --host=$DSI_IP --port=$DSI_PORT $SOL_MANAGER_OPTS
 
 docker-compose run -v $SOL_DIR:/dropins dsi-runtime /dsi-cmd connectivityManager deploy remote /dropins/simple_solution-0.0.esa /dropins/in-connectivity-server-configuration.xml --host=$DSI_IP --port=$DSI_PORT $SOL_MANAGER_OPTS
