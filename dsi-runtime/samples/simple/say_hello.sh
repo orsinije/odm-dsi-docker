a96d8f70795a31a6584fb3c19274a0b9230ec63d
Jean-Philippe Orsini <orsinije@fr.ibm.com>
2018-04-16 16:47:24 +0200

    correct json payload when an person name is passed

--- dsi-runtime/samples/simple/say_hello.sh
+++ dsi-runtime/samples/simple/say_hello.sh
@@ -7,7 +7,7 @@ URL="https://localhost:9443/in/simple_json"
 cp ./say_hello.json /tmp/$$.json
 
 if [ ! -z "$1" ]; then
-        sed -i 's/john.doe/$1/g' /tmp/$$.json
+        sed -i "s/john.doe/${1}/g" /tmp/$$.json
 fi
 
 curl -k -H "Content-Type: application/json" -d @/tmp/$$.json -X POST $URL
